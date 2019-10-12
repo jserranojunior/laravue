a944d82db28c3cba9101e1b0402583eed1d89b0b
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2019-10-12 13:34:22 -0300

    First Commit

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Bem vindo ao Vue"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  mounted(){
+    // eslint-disable-next-line
+    console.log("Funcionando")
+  }
+}
+</script>
